d63352df3bb065bb05b8e0fe6821afa2a45e2b6c
Dale Sublett <djsublett04@gmail.com>
2019-04-01 17:02:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ main.addEventListener('click', function(event) {
   alert('I was clicked');
 })
 
+let divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
